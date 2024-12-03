3a20e43e73d1f7edfe7a75bf4ff4d74954719390
Abel Alazar Dirar <aadlinux358@gmail.com>
2024-12-03 17:53:00 +0300

    fix: unique constraint on duplicate todo items.

--- python_todo/db.py
+++ python_todo/db.py
@@ -1,4 +1,5 @@
 """todo app database module."""
+
 import pathlib
 from datetime import datetime
 from sqlalchemy import String, create_engine, ForeignKey, UniqueConstraint, func
@@ -8,36 +9,39 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 
-app_dir = pathlib.Path.home() / '.pytodo'
+app_dir = pathlib.Path.home() / ".pytodo"
 app_dir.mkdir(exist_ok=True)
 
 
-
-engine = create_engine(f'sqlite:///{app_dir}/todo_db.sqlite')
+engine = create_engine(f"sqlite:///{app_dir}/todo_db.sqlite")
 Session = sessionmaker(engine)
+
 
 class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
     create_date: Mapped[datetime] = mapped_column(insert_default=func.now())
 
+
 class TodoUser(Base):
     """Todo user model."""
-    __tablename__ = 'todo_user'
+
+    __tablename__ = "todo_user"
     username: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
     password_hash: Mapped[str]
 
-    todos: Mapped[list['Todo']] = relationship(back_populates="todo_user")
+    todos: Mapped[list["Todo"]] = relationship(back_populates="todo_user")
+
 
 class Todo(Base):
     """Todo db model."""
-    __tablename__ = 'todo'
-    __table_args__ = (UniqueConstraint('user_id', 'title', 'done'),)
-    user_id = mapped_column(ForeignKey('todo_user.id'))
+
+    __tablename__ = "todo"
+    # __table_args__ = (UniqueConstraint('user_id', 'title', 'done'),)
+    user_id = mapped_column(ForeignKey("todo_user.id"))
     title: Mapped[str] = mapped_column(String(200), nullable=False)
     done: Mapped[bool]
 
     todo_user: Mapped["TodoUser"] = relationship(back_populates="todos")
-
 
 
 def get_db():
